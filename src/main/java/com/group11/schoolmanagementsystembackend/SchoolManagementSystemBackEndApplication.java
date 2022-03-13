13b043135422d44a4d7588934e68768c2c4e1d01
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-03-13 19:56:59 +0530

    first commit

---
+++ src/main/java/com/group11/schoolmanagementsystembackend/SchoolManagementSystemBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.group11.schoolmanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementSystemBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group11/schoolmanagementsystembackend/SchoolManagementSystemBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group11.schoolmanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementSystemBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
